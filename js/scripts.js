399ef7bbd9db6c31b6400f02486a59c117d1b7ae
Cory LaViska <cory@abeautifulsite.net>
2017-04-27 11:14:21 -0400

    Hide nav when closed to fix iOS / Safari issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 $(function() {
   'use strict';
 
-  // Toggle menu with buttons
+  // Hide the nav by default
+  $('.nav').css('display', 'none');
+
+  // Toggle nav with buttons
   $('.nav-toggle').on('click', function(event) {
     event.preventDefault();
 
-    // Don't open the nav if we're in the editor
+    // Don't open if we're in the editor
     if(window.Postleaf) return;
 
-    $('html').toggleClass('nav-open');
+    // Show it
+    $('.nav').css('display', 'block');
+
+    // Animate it on the next tick
+    setTimeout(function() {
+      $('html').toggleClass('nav-open');
+    }, 0);
+  });
+
+  // Hide the nav when closed after the transition stops
+  $('.nav').on('transitionend', function() {
+    $('html:not(.nav-open) .nav').css('display', 'none');
   });
 
   // Close menu when clicking outside it
